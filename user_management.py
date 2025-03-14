8e800f28554dbba589f4747c458a1c9c57e0dad9
CaraThang <cara.thang@icloud.com>
2025-03-15 02:10:21 +1100

    Hashed Password Patch

--- user_management.py
+++ user_management.py
@@ -13,31 +13,21 @@ def insertUser(username, password, DoB):
     con.commit()
     con.close()
 
-
-def retrieveUsers(username, password):
+def retrieveUsers(username):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
-    if cur.fetchone() == None:
-        con.close()
-        return False
-    else:
-        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
-        # Plain text log of visitor count as requested by Unsecure PWA management
-        with open("visitor_log.txt", "r") as file:
-            number = int(file.read().strip())
-            number += 1
-        with open("visitor_log.txt", "w") as file:
-            file.write(str(number))
-        # Simulate response time of heavy app for testing purposes
-        time.sleep(random.randint(80, 90) / 1000)
-        if cur.fetchone() == None:
-            con.close()
-            return False
-        else:
-            con.close()
-            return True
+    user = cur.execute("SELECT password FROM users WHERE username = ?", (username,)).fetchone()
+    con.close()
+    # Plain text log of visitor count as requested by Unsecure PWA management 
+    with open("visitor_log.txt", "r") as file:
+        number = int(file.read().strip())
+        number += 1
+    with open("visitor_log.txt", "w") as file:
+        file.write(str(number))
 
+    if user:
+        return {"password": user[0]}  # Return hashed password
+    return None # Return None if user doesn't exist
 
 def insertFeedback(feedback):
     con = sql.connect("database_files/database.db")
